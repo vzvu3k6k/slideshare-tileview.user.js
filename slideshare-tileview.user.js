c8d9039383e51de9f7599ffe94b44bef5de7f499
vzvu3k6k <vzvu3k6k@gmail.com>
2013-08-06 16:22:46 +0900

    Version bump

--- slideshare-tileview.user.js
+++ slideshare-tileview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           slideshare tileview
 // @description    Shows slides in tile
-// @version        1.0
+// @version        1.1
 // @author         vzvu3k6k
 // @match          http://www.slideshare.net/*
 // @namespace      http://vzvu3k6k.tk/
